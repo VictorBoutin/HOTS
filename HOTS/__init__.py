1ce164f875001498f343cac3f2dc4439c7b48e96
VictorBoutin <victor.boutin@univ-amu.fr>
2017-09-28 12:47:39 +0200

    add Cluster to the import

--- HOTS/__init__.py
+++ HOTS/__init__.py
@@ -14,13 +14,9 @@ A Hierarchy of event-based Time-Surfaces for Pattern Recognition
 * This code aims to replicate the paper : 'HOTS : A Hierachy of Event Based Time-Surfaces for
 Pattern Recognition' Xavier Lagorce, Garrick Orchard, Fransesco Gallupi, And Ryad Benosman'
 """
-# from .shl_scripts import *
-# from .shl_tools import *
-# from .shl_encode import *
-# from .shl_learn import *
+
 from HOTS import STS
 from HOTS import Event
 from HOTS import Monitor
-#from shl_scripts import shl_encode
-#from shl_scripts import shl_learn
-#from shl_scripts import shl_experiments
+from HOTS import Cluster
+from HOTS import Tools
