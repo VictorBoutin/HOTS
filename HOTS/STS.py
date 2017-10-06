3394ca5e56801f2d9a3654b43afd908540fc6e4c
VictorBoutin <victor.boutin@univ-amu.fr>
2017-10-06 17:58:21 +0200

    Added Maro Filter

--- HOTS/STS.py
+++ HOTS/STS.py
@@ -21,10 +21,16 @@ class STS(object):
             self.mask = np.exp(- .5 * self.radius**2 / self.R **2 / sigma**2 )
         self.mask = self.mask.reshape((1, self.mask.shape[0]*self.mask.shape[1]))
 
-    def create(self, event, stop=None):
+    #def exponential_kernel(self):
+    #    return
+
+    #def linear_kernel(self):
+
+
+    def create(self, event, stop=None, kernel='exponential'):
         self.ListPolarities = event.ListPolarities
         self.nb_polarities = len(self.ListPolarities)
-        
+
         self.width = event.ImageSize[0] + 2*self.R
         self.height = event.ImageSize[1] + 2*self.R
         self.ListOfTimeMatrix = np.zeros((self.nb_polarities, self.width,self.height))-self.initial_time
@@ -41,12 +47,20 @@ class STS(object):
         for idx_event, [addr,t,pol] in enumerate(zip(event.address,event.time,event.polarity)):
             if t<t_previous:
                 self.ListOfTimeMatrix = np.zeros((self.nb_polarities, self.width, self.height)) - self.initial_time
-
             x, y = addr + self.R
             idx_pola = self.ListPolarities.index(pol)
             self.ListOfTimeMatrix[idx_pola, x, y] = t
-            self.LocalTimeMatrix = self.ListOfTimeMatrix[:,(x-self.R):(x+self.R+1),(y-self.R):(y+self.R+1)]
-            SI = np.exp(-(t-self.LocalTimeMatrix)/self.tau).reshape((len(self.ListPolarities)* self.area))
+            #self.LocalTimeMatrix = self.ListOfTimeMatrix[:,(x-self.R):(x+self.R+1),(y-self.R):(y+self.R+1)]
+            self.LocalTimeDiff = t - self.ListOfTimeMatrix[:,(x-self.R):(x+self.R+1),(y-self.R):(y+self.R+1)]
+
+            if kernel == 'exponential':
+                #SI = np.exp(-(t-self.LocalTimeMatrix)/self.tau).reshape((len(self.ListPolarities)* self.area))
+                SI = np.exp(-(self.LocalTimeDiff)/self.tau).reshape((len(self.ListPolarities)* self.area))
+            elif kernel == 'linear':
+                mask = (self.LocalTimeDiff < self.tau)
+                SI = ((1-self.LocalTimeDiff/self.tau) * mask).reshape((len(self.ListPolarities)* self.area))
+            else :
+                print('error')
             self.Surface[idx_event,:] = SI#SI*self.mask
             t_previous = t
             if idx_event == stop:
@@ -56,3 +70,22 @@ class STS(object):
         if self.verbose != 0:
             print('Generation of SpatioTemporal Surface in ------ {0:.2f} s'.format((tac-timer)))
         return self.Surface
+    '''
+    def FilterRecent(self,event,threshold=2):
+        threshold = threshold*self.R
+        Surf = self.Surface.reshape(self.Surface.shape[0],len(event.ListPolarities),self.area)
+        filt = np.all(np.sum(Surf, axis = 2) > np.ones_like(event.ListPolarities)*threshold/len(event.ListPolarities), axis = 1)
+        self.Surface = self.Surface[filt]
+        event_output = event.filter(filt)
+
+        return event_output, filt
+    '''
+    def FilterRecent(self,event,threshold=2):
+        threshold = threshold*self.R
+        if self.verbose > 0:
+            print('threshold', threshold)
+        filt = np.sum(self.Surface, axis = 1) > threshold
+        self.Surface = self.Surface[filt]
+        event_output = event.filter(filt)
+
+        return event_output, filt
